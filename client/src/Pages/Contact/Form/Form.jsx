35f1c3bd47620fcd3e00844f0957f1ac5518ea65
karkuvelpandi <karkuvelpandi007@gmail.com>
2022-12-30 16:14:20 +0530

    ok

--- client/src/Pages/Contact/Form/Form.jsx
+++ client/src/Pages/Contact/Form/Form.jsx
@@ -1,25 +1,56 @@
 import React from 'react'
 import './Form.css'
+import Axios from 'axios'
+import { useState } from 'react'
+
 const Form = () => {
+  let [contact, setContact] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+    message: ""
+  })
+  let [submitted, setSubmitted] = useState(false)
+
+  const getData = (e) => {
+    setContact({ ...contact, [e.target.name]: e.target.value })
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    let url = `https://good-gold-ray-cape.cyclic.app/contact/create`
+    Axios.post(url, contact)
+      .then((resp) => {
+        alert("Contact submitted successfully...")
+        console.log("Contact submitted successfully...")
+        setSubmitted(true)
+      })
+      .catch(() => { })
+  }
   return <>
-     <div className="form-container">
+    <div className="form-container">
       <div className="form-left">
-      <h1>GET IN TOUCH</h1>
-      <h3 className='h3-text'>Please complete the form and we will get back to you.</h3>
+        <h1>GET IN TOUCH</h1>
+        <h3 className='h3-text'>Please complete the form and we will get back to you.</h3>
       </div>
-      <div className="form-right">
-        <label className='label'>Name*</label>
-        <input className='input' type="text" placeholder='Enter Your Name' /><br />
-        <label className='label'>Email*</label>
-        <input className='input' type="text" placeholder='Enter Your Email' /><br />
-        <label className='label'>Mobile Number*</label>
-        <input className='input' type="text" placeholder='Enter Your Mobile Number' /><br />
-        <label className='label'>Message</label>
-        <textarea className='input textarea' name="message"  cols="50" rows="4" placeholder='Enter Message'></textarea><br />
-        <button className='input btn'>Contact Now</button>
-      </div>
-     </div>
-  
+      {
+        submitted ? <><h1>Successful...!</h1></> : <>
+          <form onSubmit={submitHandler} className="form-right">
+            <label className='label'>Name*</label>
+            <input className='input' name='name' type="text" onChange={getData} placeholder='Enter Your Name' /><br />
+            <label className='label'>Email*</label>
+            <input className='input' name='email' type="email" onChange={getData} placeholder='Enter Your Email' /><br />
+            <label className='label'>Mobile Number*</label>
+            <input className='input' name='mobile' type="number" onChange={getData} placeholder='Enter Your Mobile Number' /><br />
+            <label className='label'>Message</label>
+            <textarea className='input textarea' name="message" onChange={getData} cols="50" rows="4" placeholder='Enter Message'></textarea><br />
+            <input className='input btn' type="submit" value='Contact Now' />
+          </form>
+        </>
+      }
+
+    </div>
+
   </>
 }
 
