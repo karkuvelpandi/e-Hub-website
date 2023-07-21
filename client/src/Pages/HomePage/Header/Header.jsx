5de39df0f404836723bc94bdf1d24e8b5574d6aa
Karkuvel Pandi <karkuvelpandi007@gmail.com>
2023-07-21 18:40:57 +0530

    feat: :sparkles: adds new slider

--- client/src/Pages/HomePage/Header/Header.jsx
+++ client/src/Pages/HomePage/Header/Header.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Header.css'
 import logo2 from '../../../assets/header-img.png'
+import { Slider } from '../../../components/Slider/Slider'
+import slider1 from "../../../assets/Slider/slider1.jpg"
+import slider2 from "../../../assets/Slider/slider2.jpg"
+import slider3 from "../../../assets/Slider/slider3.jpg"
 const Header = () => {
-  return <>
-     <div className='header'>
-        <div className='header-content'>
-            <img className='header-img' src={logo2} alt="" />
-            <p>CARVING FUTURE</p>
-            <p>TECHNOLOGY PROFESSIONALS</p>
-            <p>OUT OF YOUNG MINDS</p>
-        </div>
-     </div>
-  </>
-}
+   const imageArray = [slider1, slider2, slider3]
+   // const [imagesLoaded, setImagesLoaded] = useState(false);
 
+   // useEffect(() => {
+   //    // Preload images
+   //    const preloadImages = async () => {
+   //       await Promise.all(
+   //          imageArray.map((imageSrc) => {
+   //             return new Promise((resolve, reject) => {
+   //                const image = new Image();
+   //                image.onload = resolve;
+   //                image.onerror = reject;
+   //                image.src = imageSrc;
+   //             });
+   //          })
+   //       );
+   //       setImagesLoaded(true);
+   //    };
+
+   //    preloadImages();
+   // }, []);
+   const imageChild = () => {
+      return imageArray.map((img, index) => (
+         <div>
+            <img key={index} src={img} alt="" />
+            <div className=" bg-gradient-to-b from-transparent to-white absolute inset-0" />
+         </div>
+      ))
+   }
+   return <>
+      <div className='relative h-[461px]'>
+         <Slider dots arrow dotColor='blue' sliderClass="" dotContainerClass='flex gap-10 absolute top-1/2 left-1/2'>
+            {imageChild()}
+         </Slider>
+      </div>
+   </>
+}
 export default Header
