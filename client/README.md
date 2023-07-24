074b0f9ba6a607601d48be8df29f1cd91d592a0b
Karkuvel Pandi <karkuvelpandi007@gmail.com>
2023-07-24 21:41:37 +0530

    refactor: :fire: removed frontend codebase and created new repo

--- client/README.md
+++
@@ -1,1 +0,0 @@
-"# e-Hub-Frontend" 
--- client/src/redux/sagas/index.js
+++
@@ -1,7 +0,0 @@
-import { all } from "redux-saga/effects";
-
-export default function* rootSaga() {
-  yield all([
-    // all sagas which will export form all saga files.
-  ]);
-}
