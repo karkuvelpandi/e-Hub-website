c7f3b937ae5c0697edb9e510cab443e46ad47241
Karkuvel Pandi <karkuvelpandi007@gmail.com>
2023-08-23 12:54:06 +0530

    revert changes

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use(cors());
 //http logger
 app.use(morgan("tiny"));
 //reading form data
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 //connecting mongoDB
 mongoose.set("strictQuery", false);
 mongoose
